b34c12830d292cca3d93078d6a095c6bcf240e42
MY_EMAIL <porwaladisha@gmail.com>
2015-03-10 00:57:10 +0530

    about page done

--- about.php
+++ about.php
@@ -1,5 +1,42 @@
-<div class="row">
-<div class="col-md-6">
-<img src="images/882121_10151540854303980_61772062_o.jpg" class="img-responsive"></div>
-<div class="col-md-6"></div>
+
+<div class="row light-bg">
+<div class="col-sm-6 no-padding my-profile">
+
+</div>
+<div class="col-sm-6 no-padding baby-pink">
+<br>
+  <h3 class="pink margin-10">Hey There..!!!</h3>
+  <p style=" color:#f97cb3;font-family:'Comic Sans MS', cursive; font-size:16px" class="margin-10" >
+<br>
+I am Adisha, a Web designer and developer, 21 years of age. I have been working for almost 3 years now and I will be glad in helping you create your next awesome project.
+<br>
+For those that are interested in old school paper, I am pursuing a master degree in IT.
+<br>
+Aside from web development, I enjoy spending my time with my family and friends, <br>watching movies and TV series and playing games.
+</p>
+<div class="dark-bg" style="margin-top:21% ;height:40%">
+<div style="height:40%">
+<!--blank-->
+</div>
+<table class="table-condensed pink padding-10 margin-10">
+<tr>
+<th><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span> </th><th>Birthday</th>
+<td>:23/09/1993</td>
+</tr>
+<tr>
+<th><img  class="small_icon" src="images/icons/email_icon.png"/>  </th><th>Email</th>
+<td>:porwaladisha@gmail.com</td>
+</tr>
+<tr>
+<th><img  class="small_icon" src="images/icons/website_icon.png"/>  </th><th>Website</th>
+<td>:somewebaddress</td>
+</tr>
+<tr>
+<th><span class="glyphicon glyphicon-map-marker" aria-hidden="true"> </span>  </th><th>City</th>
+<td>:Indore</td>
+</tr>
+</table>
+
+</div>
+</div>
 </div>
--- css_links.php
+++ css_links.php
@@ -1,3 +1,4 @@
 <link href=<?php echo $PATH . "css/bootstrap.css" ?> rel="stylesheet" type="text/css" />
 <link href=<?php echo $PATH . "css/style.css" ?> rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="css/font-awesome.min.css">
 
